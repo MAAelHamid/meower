56e628805494f3ee968c27ef3e5a3e609ced0bd5
maaelhamid <m.a.a.elhamid@gmail.com>
2021-03-29 20:12:00 +0200

    updated

--- connect.php
+++ connect.php
@@ -1,16 +1,16 @@
 <?php 
   $dsn = "mysql:host=localhost;dbname=mew";
   $user = "root";
-  $pass = "";
+  $pass = "root";
   $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
   );
 
   try {
-    $con = new PDO($dsn,$user,$pass,$option);
+    $con = new PDO($dsn,$user,$pass);
     $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "u are connected";
   } catch(PDOException $e) {
-    // echo "Failed" . $e->getMessage();
+    echo "Failed" . $e->getMessage();
   }
 ?>
